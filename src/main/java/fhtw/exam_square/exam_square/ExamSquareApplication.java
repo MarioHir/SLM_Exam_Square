623be57c1c3a91c06e64aca1a3572374ec332c86
ic20b012 <ic20b012@technikum-wien.at>
2023-01-26 18:00:02 +0100

    [init]

---
+++ src/main/java/fhtw/exam_square/exam_square/ExamSquareApplication.java
@@ -0,0 +1,13 @@
+package fhtw.exam_square.exam_square;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamSquareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamSquareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fhtw/exam_square/exam_square/ExamSquareApplicationTests.java
@@ -0,0 +1,13 @@
+package fhtw.exam_square.exam_square;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamSquareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
